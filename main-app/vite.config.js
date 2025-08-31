d365708578677a463de84977dbca30e60a426b9b
Vikash Gupta <137061206+vik802207@users.noreply.github.com>
2025-08-31 12:30:20 +0530

    Update vite.config.js

--- main-app/vite.config.js
+++ main-app/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     federation({
       name: 'app',
       remotes: {
-        remoteApp: 'http://localhost:5001/assets/remoteEntry.js',
+        remoteApp: 'https://music-lib123.netlify.app/assets/remoteEntry.js',
       },
       shared: ['react', 'react-dom'],
     }),
